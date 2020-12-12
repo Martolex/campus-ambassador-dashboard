2f92c4365a4bb14e89dfca08f5064689fb314dfc
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-12-12 17:36:48 +0530

    remove lead button while adding lead

--- src/components/content/Leads/AddLeadsModal.js
+++ src/components/content/Leads/AddLeadsModal.js
@@ -5,6 +5,7 @@ import { get, post } from "../../../utils/requests";
 import _ from "lodash";
 import Select from "react-select/async";
 import { connect } from "react-redux";
+import { MdClose } from "react-icons/md";
 const AddLeadsModal = (props) => {
   const lead = { name: "", email: "", phoneNo: "" };
   const initialState = {
@@ -36,6 +37,11 @@ const AddLeadsModal = (props) => {
   function addNewLead() {
     setData({ ...data, leads: [...data.leads, lead] });
   }
+  function removeLead(index) {
+    return function () {
+      setData({ ...data, leads: data.leads.filter((_, idx) => idx !== index) });
+    };
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -58,6 +64,8 @@ const AddLeadsModal = (props) => {
         phoneNo,
       })),
     };
+    console.log(postData.leads);
+    return;
     try {
       const [res] = await post(LeadsApi.saveLeads, true, postData);
       if (res.errors?.duplicates?.length > 0) {
@@ -107,64 +115,83 @@ const AddLeadsModal = (props) => {
             </Col>
           </Row>
           {data.leads.map((lead, index) => (
-            <Row style={{ border: "1px solid #eee" }} className="m-1">
-              <Col md={4} xs={12}>
-                <Form.Group controlId={`lead-${index + 1}-name`}>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    value={lead.name}
-                    type="text"
-                    required
-                    onChange={(event) => {
-                      data.leads[index].name = event.target.value;
-                      setData({ ...data, leads: [...data.leads] });
-                    }}
-                  />
+            <Row className="align-items-center">
+              <Col className="p-0">
+                <Row style={{ border: "1px solid #eee" }} className="m-1">
+                  <Col md={4} xs={12}>
+                    <Form.Group controlId={`lead-${index + 1}-name`}>
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        value={lead.name}
+                        type="text"
+                        required
+                        onChange={(event) => {
+                          console.log(index);
+                          data.leads[index].name = event.target.value;
+                          setData({ ...data, leads: [...data.leads] });
+                        }}
+                      />
 
-                  <Form.Control.Feedback type="invalid">
-                    Name is required
-                  </Form.Control.Feedback>
-                </Form.Group>
-              </Col>
-              <Col md={4} xs={12}>
-                <Form.Group controlId={`lead-${index + 1}-email`}>
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    value={lead.email}
-                    type="email"
-                    required
-                    onChange={(event) => {
-                      data.leads[index].email = event.target.value;
-                      setData({ ...data, leads: [...data.leads] });
-                    }}
-                  />
+                      <Form.Control.Feedback type="invalid">
+                        Name is required
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <Form.Group controlId={`lead-${index + 1}-email`}>
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control
+                        value={lead.email}
+                        type="email"
+                        required
+                        onChange={(event) => {
+                          data.leads[index].email = event.target.value;
+                          setData({ ...data, leads: [...data.leads] });
+                        }}
+                      />
 
-                  <Form.Control.Feedback type="invalid">
-                    Enter valid E-Mail
-                  </Form.Control.Feedback>
-                </Form.Group>
-              </Col>
-              <Col md={4} xs={12}>
-                <Form.Group controlId={`lead-${index + 1}-phoneNo`}>
-                  <Form.Label>Phone No.</Form.Label>
-                  <Form.Control
-                    value={lead.phoneNo}
-                    required
-                    type="tel"
-                    minLength="10"
-                    maxLength="10"
-                    pattern="[7-9]+[0-9]+"
-                    onChange={(event) => {
-                      data.leads[index].phoneNo = event.target.value;
-                      setData({ ...data, leads: [...data.leads] });
-                    }}
-                  />
+                      <Form.Control.Feedback type="invalid">
+                        Enter valid E-Mail
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                  <Col md={4} xs={12}>
+                    <Form.Group controlId={`lead-${index + 1}-phoneNo`}>
+                      <Form.Label>Phone No.</Form.Label>
+                      <Form.Control
+                        value={lead.phoneNo}
+                        required
+                        type="tel"
+                        minLength="10"
+                        maxLength="10"
+                        pattern="[7-9]+[0-9]+"
+                        onChange={(event) => {
+                          data.leads[index].phoneNo = event.target.value;
+                          setData({ ...data, leads: [...data.leads] });
+                        }}
+                      />
 
-                  <Form.Control.Feedback type="invalid">
-                    phone No is required
-                  </Form.Control.Feedback>
-                </Form.Group>
+                      <Form.Control.Feedback type="invalid">
+                        phone No is required
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                </Row>
               </Col>
+
+              {data.leads.length > 1 && (
+                <MdClose
+                  size={25}
+                  color="white"
+                  onClick={removeLead(index)}
+                  style={{
+                    backgroundColor: "#dc3545",
+                    padding: 2,
+                    borderRadius: 20,
+                  }}
+                  className="mx-1"
+                />
+              )}
             </Row>
           ))}
           <Row className="mt-2">
